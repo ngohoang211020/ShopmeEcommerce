0ee78318b5426be98307269ffa7f8acf09b52420
ngohoang2110 <hoanggg2110@gmail.com>
2022-02-21 17:57:20 +0700

    Write config authen

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -47,6 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http	
 			.authorizeRequests()
 			.antMatchers("/users/**").hasAuthority("Admin")
+			.antMatchers("/categories/**").hasAnyAuthority("Admin","Editor")
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
